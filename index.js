51c0aa818720df2fc2d704cde1ca6be8e7958e52
Mauricio Gomez <gomez.work94@gmail.com>
2019-03-15 15:08:57 -0700

    update

---
+++ index.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    $("#section-1").hide();
+    $("#section-2").hide();
+    $("#section-3").hide();
+    $("#git").hide();
+    $("#a").hide();
+    $("#b").hide();
+    $("#c").hide();
+    $("#d").hide();
+    $("#gomez").click(function () {
+        $("#section-1").toggle(800);
+        $("#section-2").hide();
+        $("#section-3").hide();
+        $("#git").toggle(4000);
+    });
+    $("#gomez").hover(function () {
+        $("#a").show();
+        $("#d").hide();
+        $("#b").hide();
+        $("#c").hide();
+        $("#e").hide();
+    });
+    $("#skills").click(function () {
+        $("#section-2").toggle(800);
+        $("#section-1").hide();
+        $("#section-3").hide();
+    });
+    $("#skills").hover(function () {
+        $("#b").show();
+        $("#a").hide();
+        $("#d").hide();
+        $("#c").hide();
+        $("#e").hide();
+    })
+    $("#goals").click(function () {
+        $("#section-3").toggle(800);
+        $("#section-1").hide();
+        $("#section-2").hide();
+    });
+    $("#goals").hover(function () {
+        $("#c").show();
+        $("#a").hide();
+        $("#b").hide();
+        $("#d").hide();
+        $("#e").hide();
+    })
+    $("#tree").hover(function () {
+        $("#c").hide();
+        $("#a").hide();
+        $("#b").hide();
+        $("#d").show();
+        $("#e").hide();
+    })
+    $("#email").hover(function () {
+        $("#c").hide();
+        $("#a").hide();
+        $("#b").hide();
+        $("#d").hide();
+        $("#e").show();
+    })
+    $("#tree").click(function () {
+        $("body").css("background", "rgb(20, 35, 20)");
+        $("#container").css("background-image", "url(/images/forest.jpg)");
+        $("#section-1").css("background", "rgb(14, 60, 0)");
+        $("#section-2").css("background", "rgb(14, 45, 0)");
+        $("#section-3").css("background", "rgb(20, 35, 20)");
+        $("img").css("filter", "grayscale(3)");
+        $("a").css("color", "red");
+        $("a").css("font-size", "24px");
+    })
+
+})
