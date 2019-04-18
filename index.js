4bfeb2edddfa87d9fb9d9e8fd622cf33dc088bda
Mauricio Gomez <gomez.work94@gmail.com>
2019-04-18 10:46:08 -0700

    updating layout

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
     $("#c").hide();
     $("#d").hide();
     $("#gomez").click(function () {
-        $("#section-1").toggle(800);
+        $("#section-1").toggle("fast").animate({
+            left: "+=150"
+        });
         $("#section-2").hide();
         $("#section-3").hide();
         $("#git").toggle(4000);
@@ -21,7 +23,7 @@ $(document).ready(function () {
         $("#e").hide();
     });
     $("#skills").click(function () {
-        $("#section-2").toggle(800);
+        $("#section-2").toggle("fast");
         $("#section-1").hide();
         $("#section-3").hide();
     });
@@ -33,7 +35,7 @@ $(document).ready(function () {
         $("#e").hide();
     })
     $("#goals").click(function () {
-        $("#section-3").toggle(800);
+        $("#section-3").toggle("fast");
         $("#section-1").hide();
         $("#section-2").hide();
     });
@@ -60,7 +62,7 @@ $(document).ready(function () {
     })
     $("#tree").click(function () {
         $("body").css("background", "rgb(20, 35, 20)");
-
+        $("#section-4").toggle("fast");
         $("#section-1").css("background", "rgb(14, 60, 0)");
         $("#section-2").css("background", "rgb(14, 45, 0)");
         $("#section-3").css("background", "rgb(20, 35, 20)");
